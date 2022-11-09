8342eceb57f539f5e094cef7dd3480c167640e9b
Alec Missine <alec_missine@yahoo.com>
2022-11-09 11:00:20 -0500

    The front page is out

--- pages/shex.js
+++ pages/shex.js
@@ -1,39 +1,33 @@
 import Head from 'next/head'
 import Link from 'next/link';
-// See also:
-// https://nextjs.org/blog/styling-next-with-styled-jsx
+import styles from './shex.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Stellar HEX</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+  <>
+    <Head>
+      <title>Stellar HEX</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className={styles.container}>
       <main>
-        <h1 className="title">
+        <h1 className={styles.title}>
           <a href='https://stellar.org'>Stellar</a> Help Exchange
         </h1>
-
-        <p className="description">
+        <p className={styles.description}>
           An equitable distributed marketplace
-          <code>
-          </code>
         </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
+        <div className={styles.grid}>
+          <a href="https://github.com/amissine/aim" className={styles.card}>
             <h3>What Is It? &rarr;</h3>
             <p>
     Stellar Help Exchange (HEX) runs on Stellar Network.
     It is a place where Stellar users exchange all kinds of help
     for rewards. If a user does not get the help she paid for,
-    she gets her payment back. And it's FOSS...
+    she gets her payment back. And it's<code>FOSS</code>...
             </p>
           </a>
-
-          <a href="https://nextjs.org/learn" className="card">
+          <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>How Does It Work? &rarr;</h3>
             <p>
     Users make help offers and help requests, other users take them.
@@ -43,10 +37,9 @@ export default function Home() {
     more...
             </p>
           </a>
-
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
+            className={styles.card}
           >
             <h3>Why Should I Care? &rarr;</h3>
             <p>
@@ -54,8 +47,7 @@ export default function Home() {
     Then see if you like it. Click here to join the discussion on Discord...
             </p>
           </a>
-
-          <Link href={'/posts/first-post'} className="card">
+          <Link href={'/posts/first-post'} className={styles.card}>
             <h3>Ok, How Do I Join? &rarr;</h3>
             <p>
     Add Stellar wallet to your browser, get the assets - and you're in! 
@@ -64,7 +56,6 @@ export default function Home() {
           </Link>
         </div>
       </main>
-
       <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -72,155 +63,10 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo}/>
         </a>
       </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: baseline;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 1400px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 20%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
+  </>
   )
 }
---
+++ posts/shex-is-out.md
@@ -0,0 +1,6 @@
+---
+title: 'The Stellar HEX Front Page Is Out'
+date: '2022-11-09'
+---
+
+The front page of Stellar HEX demo is now available [here](https://aim-753.pages.dev/shex). All its links are yet incorrect, but I hope the wording will not change much. Speaking of its style, I borrowed it from the starter template for [Learn Next.js](https://nextjs.org/learn) - with thanks! And I chose to use CSS Modules instead of Styled JSX.
