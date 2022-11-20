4f7fad3f6ceeb66f6f9b1d602e27eba4ad1c6553
Alec Missine <alec_missine@yahoo.com>
2022-11-20 09:23:05 -0500

    Add hex prop to custom network

--- shex/index.js
+++ shex/index.js
@@ -1,8 +1,8 @@
-import { stellarNetworks, } from '../foss/stellar-networks.js' // {{{1
-import { Account, } from '../foss/stellar-account.js'
+import { stellarNetworks, } from '../foss/stellar-networks.mjs' // {{{1
+import { Account, } from '../foss/stellar-account.mjs'
 
 async function setup (state, setState) { // {{{1
-  if (!state.connected) { // {{{2
+  if (!state.connected || !window.StellarSdk) { // {{{2
     return;
   } // }}}2
   console.log('setup', state)
@@ -18,16 +18,12 @@ async function setup (state, setState) { // {{{1
       */
       await state.server.loadAccount(state.account).then(async loaded => { // {{{3
         setState(p => Object.assign({}, p, { loaded }))
-        let user = new Account(
-          { loaded, network: state.network, server: state.server }
-        )
+        let user = new Account({ loaded })
         console.log('user loaded', user)
 
         if (user.trusts(state.network.hex.assets)) {
           return;
         }
-        console.log('TODO make user trust HEX assets')
-
         for (let asset of state.network.hex.assets) {
           await user.trust(asset).sign(window.freighterApi)
             .then(xdr => user.submit({ xdr })).catch(e => console.error(e))
@@ -39,8 +35,8 @@ async function setup (state, setState) { // {{{1
     setState(p => {
       let hex = p.network.hex
       hex.assets = [
-        new state.sdk.Asset('ClawableHexa', hex.issuer),
-        new state.sdk.Asset('HEXA', hex.issuer),
+        new state.sdk.Asset('ClawableHexa', hex.issuerClawableHexa),
+        new state.sdk.Asset('HEXA', hex.issuerHEXA),
       ]
       return Object.assign({}, p, { server });
     })
@@ -54,6 +50,7 @@ async function setup (state, setState) { // {{{1
   }).then(name => {
     let network = stellarNetworks().filter(v => v.name == name)[0]
     setState(p => Object.assign({}, p, { account, network }))
+    window.StellarNetwork = network
   }); // }}}2
 }
 
